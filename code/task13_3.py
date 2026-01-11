b5af4e85223ce4367aa7e9469f3ab273aa3fe386
Natale Mastrogiacomo <natanmast@gmail.com>
2026-01-11 14:22:51 +0100

    feat: task13 all generated codes

---
+++ code/task13_3.py
@@ -0,0 +1,10 @@
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
+def make_palindrome(string: str) -> str:
+    longest_palindrome_suffix = ''
+    for i in range(len(string), -1, -1):
+        if is_palindrome(string[i:]):
+            longest_palindrome_suffix = string[i:]
+            break
+    return string + longest_palindrome_suffix[::-1]
