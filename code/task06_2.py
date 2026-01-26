78f30b9f216cc92e1ac5066c1da8c6c2c984d104
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-01-26 11:52:04 +0100

    Improve odd_position function with docstring
    Refactor odd_position function to include a docstring and simplify logic.

--- code/task06_2.py
+++ code/task06_2.py
@@ -2,9 +2,10 @@ from typing import List
 
 
 def odd_position(nums: List[int]) -> bool:
-    result = True
+    """ Write a python function to check whether every odd index contains odd numbers of a given list. """
+
     for i in range(len(nums)):
-        if i % 2 != 0:
-            if nums[i] % 2 == 0:
-                result = False
-    return result
+        if i % 2 != 0:  # Check if the index is odd
+            if nums[i] % 2 == 0:  # Check if the number at the odd index is even
+                return False
+    return True
