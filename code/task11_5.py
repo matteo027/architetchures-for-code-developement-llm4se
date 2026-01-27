ec35418afeba482f87750ce37ab55f5d48829dd0
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-01-27 21:12:38 +0100

    Delete code/task11_5.py

--- code/task11_5.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-def mean_absolute_deviation(numbers: List[float]) -> float:
-    mean = sum(numbers) / len(numbers)
-    total_diff = 0
-    for num in numbers:
-        total_diff += abs(num - mean)
-    return total_diff / len(numbers)
