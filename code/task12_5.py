9b144651aec2ffb02f2450828076733135d37136
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-01-27 21:13:48 +0100

    Delete code/task12_5.py

--- code/task12_5.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-def filter_by_substring(strings: List[str], substring: str) -> List[str]:
-    result = []
-    for string in strings:
-        if substring in string:
-            result.append(string)
-    return result
